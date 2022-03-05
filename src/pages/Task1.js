397a42d753dedef54d45e6038d9068e61fefb5f2
JST1703 <johan.stettler@gmail.com>
2022-03-05 13:16:00 +0100

    update task 1

--- src/pages/Task1.js
+++ src/pages/Task1.js
@@ -83,12 +83,15 @@ function Task1() {
   return (
     <div className="main">
       <h1>Aufgabe 1: Prüfziffer und Fehlererkennung 1</h1>
+
       <div className="space"></div>
+
       <div className="task">
         <div className="taskLeft">
           <p>
-            Gegeben sind Zahlenfolgen (hier in Schwarz) und deren Prüfziffern
-            (hier in Rot). Egänzen Sie anhand der Beipiele die fehlenden
+            Gegeben sind Zahlenfolgen, bestehend aus den Ziffern 1 bis 9, und
+            deren <span style={{ color: "red" }}>{"Prüfziffern "}</span>.
+            Egänzen Sie anhand der gegebenen Beipielen die fehlenden
             Prüfziffern. Die Lösung besteht immer aus zwei Ziffern (bsp. 09
             statt 9).
           </p>
@@ -113,6 +116,7 @@ function Task1() {
           ))}
         </div>
       </div>
+
       {wrongAnswersA >= minWrongAnswers && (
         <div className="task">
           <div className="space"></div>
@@ -126,6 +130,7 @@ function Task1() {
           />
         </div>
       )}
+
       <div className="space"></div>
       <div className="task">
         <button
@@ -136,6 +141,7 @@ function Task1() {
           <p>Aufgabe überspringen</p>
         </button>
       </div>
+
       {correctAnswersA >= numberOfTasksA && (
         <div className="task">
           <div className="space"></div>
@@ -160,8 +166,16 @@ function Task1() {
               }}
             />
           </div>
+          <button
+            onClick={() => {
+              setTaskStateB(true);
+            }}
+          >
+            <p>Aufgabe überspringen</p>
+          </button>
         </div>
       )}
+
       {taskStateB && (
         <div className="task">
           <div className="space"></div>
@@ -177,17 +191,188 @@ function Task1() {
             optionNo={<span>Nein</span>}
             textOnCorrect={
               <p>
-                Wenn eine Ziffer falsch ist, dann stimmt die Summe nicht mehr
-                mit der Prüfziffer überein.
+                Wenn eine Ziffer in der Folge falsch übertragen worden ist, dann
+                stimmt die Summe nicht mehr mit der Prüfziffer überein. Das ist
+                die Idee der Prüfziffer.
               </p>
             }
             textOnWrong={
               <p>
-                Wenn eine Ziffer falsch ist, dann stimmt die Summe nicht mehr
-                mit der Prüfziffer überein.
+                Wenn eine Ziffer in der Folge falsch übertragen worden ist, dann
+                stimmt die Summe nicht mehr mit der Prüfziffer überein. Das ist
+                die Idee der Prüfziffer.
               </p>
             }
             callerFunction={() => handleTaskStateC(0)}
+          />
+        </div>
+      )}
+
+      {taskStateC[0] && (
+        <div className="task">
+          <div className="space"></div>
+          <YN
+            question={
+              <p>
+                Wenn die Prüfziffer nicht korrekt ist, dann muss die Zahlenfolge
+                einen Fehler beinhalten.
+              </p>
+            }
+            solution={0}
+            optionYes={<span>Ja</span>}
+            optionNo={<span>Nein</span>}
+            textOnCorrect={
+              <p>
+                Ein Übertragungsfehler kann auch in der Prüfziffer selbst
+                auftreten, auch wenn die Zahlenfolge fehlerfrei übertragen
+                worden ist. Man muss dennoch von einer Fehlübertragung ausgehen.
+              </p>
+            }
+            textOnWrong={
+              <p>
+                Ein Übertragungsfehler kann auch in der Prüfziffer selbst
+                auftreten, auch wenn die Zahlenfolge fehlerfrei übertragen
+                worden ist. Man muss dennoch von einer Fehlübertragung ausgehen.
+              </p>
+            }
+            callerFunction={() => handleTaskStateC(1)}
+          />
+        </div>
+      )}
+
+      {taskStateC[1] && (
+        <div className="task">
+          <div className="space"></div>
+          <YN
+            question={
+              <p>
+                Häufig geschehen auch Tippfehler beim Mensch, z.B. dass man zwei
+                Ziffern vertauscht (z.B. 73 statt 37). Kann die Prüfsu,e solche
+                fehler erkennen?
+              </p>
+            }
+            solution={0}
+            optionYes={<span>Ja</span>}
+            optionNo={<span>Nein</span>}
+            textOnCorrect={
+              <p>
+                Die Prüfsumme ist die Summe der einzelnen Zahlen. Es spielt
+                keine Rolle, in welcher Reihenfolge man die Zahlen addiert (7 +
+                3 = 3 + 7). Solche Fehler bleiben desswegen unerkannt.
+              </p>
+            }
+            textOnWrong={
+              <p>
+                Die Prüfsumme ist die Summe der einzelnen Zahlen. Es spielt
+                keine Rolle, in welcher Reihenfolge man die Zahlen addiert (7 +
+                3 = 3 + 7). Solche Fehler bleiben desswegen unerkannt.
+              </p>
+            }
+            callerFunction={() => handleTaskStateC(2)}
+          />
+        </div>
+      )}
+
+      {taskStateC[2] && (
+        <div className="task">
+          <div className="space"></div>
+          <YN
+            question={
+              <p>
+                Sollte eine Ziffer wegen einer Fehlübertragung falsch sein, dann
+                erkennen wir mit der Prüfsumme, welche Ziffer das ist.
+              </p>
+            }
+            solution={0}
+            optionYes={<span>Ja</span>}
+            optionNo={<span>Nein</span>}
+            textOnCorrect={
+              <p>
+                Die Prüfsumme kann nur erkennen, ob ein Fehler in der
+                Übertragung vorgefallen ist, allerdings nicht bei welcher
+                Ziffer.
+              </p>
+            }
+            textOnWrong={
+              <p>
+                Die Prüfsumme kann nur erkennen, ob ein Fehler in der
+                Übertragung vorgefallen ist, allerdings nicht bei welcher
+                Ziffer.
+              </p>
+            }
+            callerFunction={() => handleTaskStateC(3)}
+          />
+        </div>
+      )}
+
+      {taskStateC[3] && (
+        <div className="task">
+          <div className="space"></div>
+          <YN
+            question={
+              <p>
+                Angenommen bei der Übertragung treten zwei Fehler auf, so dass
+                nun 2 Ziffern falsch sind. Erkennt man das mit der Prüfsumme?
+              </p>
+            }
+            solution={0}
+            optionYes={<span>Ja</span>}
+            optionNo={<span>Nein</span>}
+            textOnCorrect={
+              <p>
+                Eine Ziffer könnte um den Betrag x höher sein und eine andere
+                Ziffer um den gleichen Betrag x tiefer. Die Summe der Ziffern
+                bleibt dennoch gleich, was gleich der Prüfziffer ist.
+              </p>
+            }
+            textOnWrong={
+              <p>
+                Eine Ziffer könnte um den Betrag x höher sein und eine andere
+                Ziffer um den gleichen Betrag x tiefer. Die Summe der Ziffern
+                bleibt dennoch gleich, was gleich der Prüfziffer ist. Angenommen
+                die ursprüungliche Zahlenfolge wäre 234 und die Prüfsumme 09.
+                Zwei Fehler in der Folge könnte uns 531 geben, wobei dessen
+                Prüfsumme auch 09 ist. Das würden wir fälschlicherweise als
+                fehlerfreie Übertragung betrachten.
+              </p>
+            }
+            callerFunction={() => handleTaskStateC(4)}
+          />
+        </div>
+      )}
+
+      {taskStateC[4] && (
+        <div className="task">
+          <div className="space"></div>
+          <YN
+            question={
+              <p>
+                Wenn durch ein Fehler in der Übertragung eine Ziffer in der
+                Folge ausgelassen wird, kann man das mit der Prüfziffer
+                erkennen?
+              </p>
+            }
+            solution={1}
+            optionYes={<span>Ja</span>}
+            optionNo={<span>Nein</span>}
+            textOnCorrect={
+              <p>
+                Die Ziffern liegen zwischen 1 und 9. Würde eine ausgelassen
+                werden, so stimmt die Summe der erhaltenen Folge nicht mit der
+                Prüfziffer überein.
+              </p>
+            }
+            textOnWrong={
+              <p>
+                Die Ziffern liegen zwischen 1 und 9. Würde eine ausgelassen
+                werden, so stimmt die Summe der erhaltenen Folge nicht mit der
+                Prüfziffer überein. Sollte 0 auch eine mögliche Ziffer sein,
+                dann haben Sie recht. Die Folgen 24104 und 2414 haben die
+                gleiche Prüfsumme. Das Entfallen der 0 würde nicht als
+                Übertragungsfehler aufgefasst werden.
+              </p>
+            }
+            callerFunction={() => {}}
           />
         </div>
       )}
